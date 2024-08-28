648567c43907e9164d99c05592c8f293c311d67b
flexystock <flexystock@gmail.com>
2024-08-28 12:39:43 +0200

    Creacion del metodo getAllClients
    creamos el metodo para obtener TODOS los clientes que haya en BBDD

--- src/Entity/Main/Client.php
+++ src/Entity/Main/Client.php
@@ -6,7 +6,7 @@ use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Uid\UuidV4;
 
 #[ORM\Entity(repositoryClass: App\Repository\ClientRepository::class)]
-#[ORM\Table(name: 'clients')]
+#[ORM\Table(name: 'client')]
 class Client
 {
     #[ORM\Id]
@@ -14,31 +14,11 @@ class Client
     private string $uuid;
 
     #[ORM\Column(type: 'string', length: 50, unique: true, nullable: false)]
-    private string $name;
-
-    #[ORM\Column(type: 'string', length: 50, nullable: true)]
-    private ?string $server = null;
+    private string $clientName;
 
     #[ORM\Column(type: 'string', length: 100, nullable: true)]
-    private ?string $exclusiveHost = null;
+    private ?string $databaseName= null;
 
-    #[ORM\Column(type: 'string', length: 100, nullable: true)]
-    private ?string $exclusiveUser = null;
-
-    #[ORM\Column(type: 'string', length: 20, nullable: true)]
-    private ?string $exclusivePassword = null;
-
-    #[ORM\Column(type: 'string', length: 250, nullable: false)]
-    private string $scheme;
-
-    #[ORM\Column(type: 'boolean', options: ['default' => true])]
-    private bool $active = true;
-
-    #[ORM\Column(type: 'boolean', options: ['default' => false])]
-    private bool $ttnSecureByIp = false;
-
-    #[ORM\Column(type: 'integer', nullable: true)]
-    private string $port;
 
     public function __construct()
     {
@@ -58,101 +38,12 @@ class Client
 
     public function getName(): ?string
     {
-        return $this->name;
+        return $this->clientName;
     }
 
     public function setName(string $name): self
     {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function getServer(): ?string
-    {
-        return $this->server;
-    }
-
-    public function setServer(?string $server): self
-    {
-        $this->server = $server;
-        return $this;
-    }
-
-    public function getExclusiveHost(): ?string
-    {
-        return $this->exclusiveHost;
-    }
-
-    public function setExclusiveHost(?string $exclusiveHost): self
-    {
-        $this->exclusiveHost = $exclusiveHost;
-        return $this;
-    }
-
-    public function getExclusiveUser(): ?string
-    {
-        return $this->exclusiveUser;
-    }
-
-    public function setExclusiveUser(?string $exclusiveUser): self
-    {
-        $this->exclusiveUser = $exclusiveUser;
-        return $this;
-    }
-
-    public function getExclusivePassword(): ?string
-    {
-        return $this->exclusivePassword;
-    }
-
-    public function setExclusivePassword(?string $exclusivePassword): self
-    {
-        $this->exclusivePassword = $exclusivePassword;
-        return $this;
-    }
-
-    public function getScheme(): ?string
-    {
-        return $this->scheme;
-    }
-
-    public function setScheme(string $scheme): self
-    {
-        $this->scheme = $scheme;
-        return $this;
-    }
-
-    public function isActive(): ?bool
-    {
-        return $this->active;
-    }
-
-    public function setActive(bool $active): self
-    {
-        $this->active = $active;
-        return $this;
-    }
-
-    public function isTtnSecureByIp(): ?bool
-    {
-        return $this->ttnSecureByIp;
-    }
-
-    public function setTtnSecureByIp(bool $ttnSecureByIp): self
-    {
-        $this->ttnSecureByIp = $ttnSecureByIp;
-        return $this;
-    }
-
-    public function getPort(): ?int
-    {
-        return $this->port;
-    }
-
-    public function setPort(int $port): self
-    {
-        $this->port = $port;
-
+        $this->clientName = $name;
         return $this;
     }
 }
