6a4b7da378ed8eb154acc2f88b8d96257ba5b6fc
flexystock <flexystock@gmail.com>
2024-12-21 20:06:54 +0100

    pruebas
    pruebas

--- migrations/main/027/001-create-Table-Ttn-apps.sql
+++ migrations/main/027/001-create-Table-Ttn-apps.sql
@@ -12,20 +12,6 @@ CREATE TABLE `ttn_apps` (
    `ttn_applicatioin_key_name` TINYINT(1) NOT NULL DEFAULT '1' COMMENT 'Nombre del Token para poder crear DEVICES via API',
    `expires_ttn_application_key` VARCHAR(100) NOT NULL COMMENT 'Fecha de expiracino del token',
 
-   `dev_eui` VARCHAR(16) NOT NULL COMMENT 'DevEUI del dispositivo (ej: 70B3D57E...)',
-   `join_eui` VARCHAR(16) NOT NULL COMMENT 'JoinEUI del dispositivo',
-   `app_key` VARCHAR(32) NOT NULL COMMENT 'APP KEY del dispositivo',
-   `application_id` VARCHAR(100) NOT NULL COMMENT 'application_id usada en TTN (ej: aplicacionpruebas1)',
-
-   `lorawan_version` VARCHAR(50) DEFAULT NULL COMMENT 'Versión de LoRaWAN (ej: 1.0.2)',
-   `lorawan_phy_version` VARCHAR(50) DEFAULT NULL COMMENT 'Versión física de LoRaWAN (ej: 1.0.2-b)',
-   `frequency_plan_id` VARCHAR(100) DEFAULT NULL COMMENT 'ID del plan de frecuencias (ej: EU_863_870_TTN)',
-   `supports_join` TINYINT(1) DEFAULT NULL COMMENT 'Indica si soporta join (OTAA)',
-
-
-
-
-
    `uuid_user_creation` VARCHAR(36) NOT NULL COMMENT 'UUID del usuario que crea este registro',
    `datehour_creation` DATETIME NOT NULL COMMENT 'Fecha/hora de creación del registro',
 
@@ -33,6 +19,4 @@ CREATE TABLE `ttn_apps` (
    `datehour_modification` DATETIME DEFAULT NULL COMMENT 'Fecha/hora de la última modificación',
 
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uuid_scale_UNIQUE` (`uuid_scale`),
-  UNIQUE KEY `device_id_UNIQUE` (`device_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Pool de balanzas disponibles registradas en TTN';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Aplicaciones registradas en TTN';
