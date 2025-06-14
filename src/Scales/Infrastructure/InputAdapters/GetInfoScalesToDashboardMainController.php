e6af98da06ffbed98bf80a0a7f0f31f0e9bc74dc
flexystock <flexystock@gmail.com>
2025-06-14 17:39:34 +0200

    extend abstract controller

--- src/Scales/Infrastructure/InputAdapters/GetInfoScalesToDashboardMainController.php
+++ src/Scales/Infrastructure/InputAdapters/GetInfoScalesToDashboardMainController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class GetInfoScalesToDashboardMainController
+class GetInfoScalesToDashboardMainController extends AbstractController
 {
     private GetInfoScalesToDashboardMainUseCaseInterface $getInfoScalesToDashboardMainUseCase;
     private LoggerInterface $logger;
