1575f58c00bdfc1805bb90203235d5217af8da59
flexystock <165153686+flexystock@users.noreply.github.com>
2025-06-15 22:46:01 +0200

    Add root user seeding migration

---
+++ migrations/main/035/001-Insert-Default-Root-User.sql
@@ -0,0 +1,21 @@
+INSERT INTO users (uuid_user, name, surnames, phone, email, password, is_root, active, uuid_user_creation, date_hour_creation, timezone, language)
+VALUES (
+  'a674161a-4d09-4c5a-b0d0-a61224596e44',
+  'Root',
+  'Admin',
+  123456789,
+  'root@example.com',
+  '$2y$12$uKD5lsf6pGCIZ/RMcVHb0.YBLGxY0EySK/2rlPhzi.6jjy9UB7dV.',
+  1,
+  1,
+  'a674161a-4d09-4c5a-b0d0-a61224596e44',
+  NOW(),
+  'UTC',
+  'en'
+);
+
+INSERT INTO user_role (uuid_user, role_id)
+VALUES (
+  'a674161a-4d09-4c5a-b0d0-a61224596e44',
+  (SELECT id FROM roles WHERE name='root')
+);
