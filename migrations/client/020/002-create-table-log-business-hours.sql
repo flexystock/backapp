07f27ce8ad60f5a5684ecd18f45a00e43aac70d6
flexystock <165153686+flexystock@users.noreply.github.com>
2025-10-02 19:39:00 +0200

    Add audit logs for holidays and business hours

---
+++ migrations/client/020/002-create-table-log-business-hours.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `log_business_hours` (
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `uuid_client` CHAR(36) NOT NULL,
+    `uuid_user_modification` CHAR(36) NOT NULL,
+    `data_before_modification` TEXT NOT NULL,
+    `data_after_modification` TEXT NOT NULL,
+    `date_modification` DATETIME NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
